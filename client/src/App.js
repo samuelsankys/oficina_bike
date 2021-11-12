46b4b48cb014938a19910dea84d2e7fa7d6834f8
samuelsankys <ssankys3@gmail.com>
2021-11-12 09:14:12 -0300

    initial configure  server

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Opa
+      <h1>Nagan Bikes</h1>
+      <div className="form">
+        <label for="">Movie name:</label>
+        <input type="text"
+          class="form-control" name="movieName" id="" aria-describedby="helpId" placeholder="" />
+        <label for="">Review</label>
+        <input type="text"
+          class="form-control" name="movieName" id="" aria-describedby="helpId" placeholder="" />
+
+          <button>Submit</button>
+      </div>
+      
     </div>
   );
 }
